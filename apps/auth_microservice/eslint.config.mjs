aafe4d90360e4f051a9d039036adfb963b20d71c
Roman Ananyev <romaananyev8@gmail.com>
2026-05-18 13:58:34 +0300

    eslint updates

---
+++ apps/auth_microservice/eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs', 'dist/**', 'src/generated/**'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+);
